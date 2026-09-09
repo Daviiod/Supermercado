157c20fbde04ece3570ea32b2b608e6ef8dc98df
Daviiod <davioliveiraduarte2018@gmail.com>
2026-09-09 11:32:29 -0300

    projeto BD

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Supermercado
-# Supermercado
+Super, merca do.
--- app/DataBase.php
+++ app/DataBase.php
@@ -7,42 +7,87 @@ class DataBase{
 
     private const HOST = 'localhost';
     private const USER = 'root';
-    private const PASSWORD = '456';
+    private const PASSWORD = '123';
     private const DBNAME = 'supermercado';
 
     private $connection;
     private $table;
 
-    public function __contruct($table){
+    public function __construct($table = null){
         $this->table = $table;
         $this->setConnection();
     }
     //metodo que cria conexao com o banco
     public function setConnection(){
-        $connection = new PDO('mysql:host='.self::HOST.';dbname='.self::DBNAME,self::USER,self::PASSWORD);
+        $this->connection = new PDO('mysql:host='.self::HOST.';dbname='.self::DBNAME,self::USER,self::PASSWORD);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     }
-    //metodo que cadastra/insere dados
+    public function execute($query, $values = null){
+         try{
+            echo'<pre>';
+            print_r($query); //query são os comandos sql.
+            echo'</pre>';
+            $statement = $this->connection->prepare($query);
+            $statement->execute($values);
+            return $statement;
+         }catch(PDOException $e){
+            die('ERROR: '.$e);
+         }
+    }
+
     public function insert($array){
-    //extrair as chaves do array
-    $fields = array_keys($array);
-    //criar um array com valores =  ?
-    $binds = array_pad([],count($array),'?');
-    //criar a query
-    $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES('.implode(',',$binds).')';
-    //executar a query
-    //$this->execute($query, array_values($array));
-    return $this->connection->lastInsertId();
+        $fields = array_keys($array); //extrair as chaves do array
+        $binds = array_pad([],count($array),'?'); //criar um array com valores =  ?
+        $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')'; //criar a query
+        $this->execute($query, array_values($array)); //executar a query
+        return $this->connection->lastInsertId();
+    }
+    public function select($where = null,$order=null,$limit=null,$fields='*'){
+
+        $where = strlen($where) ? 'WHERE '.$where : '';
+        $order = strlen($order) ? 'ORDER BY '.$order : '';
+        $limit = strlen($limit) ? 'LIMIT '.$limit : '';
+        $query = 'SELECT '.$fields.' FROM '. $this->table.' '.$where.' '.$order.' '.$limit;
+        return $this->execute($query);
+    }
+    public function update($where, $array){
+        $fields = array_keys($array); //extrair as chaves do array
+        $query = ' UPDATE '.$this->table.' SET '.implode('=?,',$fields).' WHERE '.$where; //montar query
+        $this->execute($query, array_values($array)); //executar a query
+        return true;
+    }
+    public function delete($where){
+        $query = 'DELETE FROM '.$this->table.' WHERE '.$where;
+        $this->execute($query);
+        return true;
     }
 }
 
 $db = new DataBase('fornecedor');
-$db->insert([
-    'nome'=>'Saka Max',
-    'cnpj'=>'324',
-    'telefone'=>'(63) 9 9745-5148',
-    'email'=>'saka@max.com',
-    'endereco'=>'Rua Saka Max']);
-?>
+$g = $db->select(null,'cnpj',2,'nome, cnpj')->fetchAll(PDO::FETCH_CLASS);
+echo'<pre>';
+print_r($g);
+echo'</pre>';
+$db->delete('id=1');
+$g = $db->select(null,'cnpj',2,'nome, cnpj')->fetchAll(PDO::FETCH_CLASS);
+echo'<pre>';
+print_r($g);
+echo'</pre>';
 
-git config --global user.name "Daviiod"
-git config --global user.email "davioliveiraduarte2018@gmail.com"
+
+
+
+
+// $db->update('id=4',
+// [
+//     'nome'=>'Sneak Fox',
+//     'cnpj'=>'666',
+//     'telefone'=>'(63) 9 5425-3234',
+//     'email'=>'cobra@esperta.com',
+//     'endereco'=>'Rua Cobra'
+// ]);
+// $f = $db->select(null,'cnpj',2,'nome, cnpj')->fetchAll(PDO::FETCH_CLASS);
+// echo'<pre>';
+// print_r($f);
+// echo'</pre>';
+?>
